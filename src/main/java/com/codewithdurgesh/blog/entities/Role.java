84097e86cfb64bc45c3627dcecb76bdde01aa662
B@blu1234 <bablubambal1999@gmail.com>
2022-09-23 21:45:25 +0530

    basic custom authentication

---
+++ src/main/java/com/codewithdurgesh/blog/entities/Role.java
@@ -0,0 +1,24 @@
+package com.codewithdurgesh.blog.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+@Getter
+@Setter
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+
+}
